f9880f5332a672541f2cf418ef3cc013feab5adf
Jeff Taylor <jeftaylo@redhat.com>
2022-11-07 08:13:29 -0500

    update to quarkus.platform.version 2.13.3.Final

--- vehicle-decision-service/src/main/java/com/example/vehicle/rules/VehicleUnitData.java
+++ vehicle-decision-service/src/main/java/com/example/vehicle/rules/VehicleUnitData.java
@@ -3,9 +3,9 @@ package com.example.vehicle.rules;
 import com.example.vehicle.datamodel.Vehicle;
 import com.example.vehicle.datamodel.Car;
 import com.example.vehicle.datamodel.Truck;
-import org.kie.kogito.rules.DataSource;
-import org.kie.kogito.rules.DataStore;
-import org.kie.kogito.rules.RuleUnitData;
+import org.drools.ruleunits.api.DataSource;
+import org.drools.ruleunits.api.DataStore;
+import org.drools.ruleunits.api.RuleUnitData;
 
 @lombok.Getter
 @lombok.Setter
